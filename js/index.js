68cd08b2d6dac7ad2ea0408a710c67a4097355cf
Mbasa Cokile <60657016+mbasacokile7@users.noreply.github.com>
2022-01-30 16:13:02 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,43 +6,5 @@ window.onload = function() {
     maxParticles: 150,
     sizeVariations: 4,
   });
-  responsive: [
-    {
-      breakpoint: 
-768
-,
-      options: {
-        maxParticles: 
-200
-,
-        color: 
-'#48F2E3'
-,
-        connectParticles: 
-false
-      }
-    }, {
-      breakpoint: 
-425
-,
-      options: {
-        maxParticles: 
-100
-,
-        connectParticles: 
-true
-      }
-    }, {
-      breakpoint: 
-320
-,
-      options: {
-        maxParticles: 
-0
- 
-// disables particles.js
-      }
-    }
-  ]
-});
 };
+ 

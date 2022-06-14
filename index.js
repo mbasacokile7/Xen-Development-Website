b4f6ad3626350fe2b91758343f4972578d0606ac
Mbasa Cokile <60657016+mbasacokile7@users.noreply.github.com>
2022-06-14 18:38:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    connectParticles: true,
+    color: '#FFFFFF',
+    maxParticles: 150,
+    sizeVariations: 4,
+  });
+};
